bef2e9372ecf7758fbbc5b85601217b6a73bf6e6
aditi-lab-09 <adytea.09@gmail.com>
2026-05-01 23:06:00 +0530

    Update area.java

--- area.java
+++ area.java
@@ -1,7 +1,7 @@
 public class area {
     public static void main(String[] args) {
-        double length = 10;   // You can change the value
-        double width = 5;     // You can change the value
+        double length = 10; 
+        double width = 5;     
 
         double area = length * width;
         double perimeter = 2 * (length + width);
