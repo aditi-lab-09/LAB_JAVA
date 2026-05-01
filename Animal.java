0f1bebacbfdc65af2986d7c10480c220f4f56630
aditi-lab-09 <adytea.09@gmail.com>
2026-05-01 22:56:02 +0530

    Animal.java

--- Animal.java
+++ Animal.java
@@ -1,29 +1,23 @@
-// Parent class
+
 class Animal {
     void sound() {
         System.out.println("Animal makes a sound");
     }
 }
 
-// Child class
 class Dog extends Animal {
-    // Overriding method
     void sound() {
         System.out.println("Dog barks");
     }
 }
 
-// Main class
+
 public class Main {
     public static void main(String[] args) {
 
         Animal a;
-
-        // Parent object
         a = new Animal();
         a.sound();
-
-        // Child object (overriding in action)
         a = new Dog();
         a.sound();
     }
